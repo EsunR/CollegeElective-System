de12f5f801f386c3173fb3354cd38390dec360f1
EsunR <641411169@qq.com>
2019-03-20 11:28:55 +0800

    init style

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <transition>
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
+</style>
 
+<style lang="scss">
+html {
+  height: 100%;
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+}
+body {
+  height: 100%;
+  background-color: #f4f5f5 !important;
+}
+.v-enter {
+  opacity: 0;
+  transform: translateX(100%);
+}
+.v-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+  position: absolute;
+}
+.v-enter-active,
+.v-leave-active {
+  transition: all 1s ease;
+}
 </style>
 
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './components/Home.vue'
+// import global from './common.vue'
 Vue.use(Router)
 
 var router = new Router({
@@ -12,12 +13,12 @@ var router = new Router({
 })
 
 router.beforeEach((to, from, next) => {
-  if(localStorage.getItem('token')){
+  if (localStorage.getItem('token')) {
     console.log("get success");
     next();
-  }else{
+  } else {
     console.log("no token!");
-    window.location.href = '/login.html'
+    // window.location.href = global.login_location
     next();
   }
 })
